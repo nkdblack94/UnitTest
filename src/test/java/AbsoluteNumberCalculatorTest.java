ca72a29ad4964bfcbf589c05508a86bcfde72006
nkdblack94 <nkdblack1294@gmail.com>
2020-06-18 13:20:02 +0700

    thực hành unit test

---
+++ src/test/java/AbsoluteNumberCalculatorTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
+class AbsoluteNumberCalculatorTest {
+
+    @Test
+    void testFindAbsolute(){
+        int number = 0;
+        int expected = 0;
+
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testFindAbsolute1(){
+        int number = -1;
+        int expected = 1;
+
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testFindAbsoluteNegative1() {
+        int number = -1;
+        int expected = 1;
+
+        int result = AbsoluteNumberCalculator.findAbsolute(number);
+        assertEquals(expected, result);
+    }
+}
---
+++ src/test/java/SimpleCalculatorTest.java
@@ -0,0 +1,87 @@
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertEquals;
+
+public class SimpleCalculatorTest {
+
+    @Test
+    public void testAdd0And0() {
+        int first = 0;
+        int second = 0;
+        int expected = 0;
+
+        int result = SimpleCalculator.add(first, second);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testAdd1And0() {
+        int first = 1;
+        int second = 0;
+        int expected = 1;
+
+        int result = SimpleCalculator.add(first, second);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testAdd0And1() {
+        int first = 0;
+        int second = 1;
+        int expected = 1;
+
+        int result = SimpleCalculator.add(first, second);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testAdd5And3() {
+        int first = 5;
+        int second = 3;
+        int expected = 8;
+
+        int result = SimpleCalculator.add(first, second);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testSub0and0() {
+        int first = 0;
+        int second = 0;
+        int expected = 0;
+
+        int result = SimpleCalculator.sub(first, second);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testSub1and0() {
+        int first = 1;
+        int second = 0;
+        int expected = 1;
+
+        int result = SimpleCalculator.sub(first, second);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testSub0and1() {
+        int first = 0;
+        int second = 1;
+        int expected = -1;
+
+        int result = SimpleCalculator.sub(first, second);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testSub5and3() {
+        int first = 5;
+        int second = 3;
+        int expected = 2;
+
+        int result = SimpleCalculator.sub(first, second);
+        assertEquals(expected, result);
+    }
+}
